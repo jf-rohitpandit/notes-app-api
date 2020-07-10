953042ec37bd15c7d86f454f2e70fe78cc9e973a
rohit <rohit0311@outlook.com>
2020-07-10 16:00:51 +0530

    all routes done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 require("./database/mongoose");
-const User = require("./models/user");
-const Task = require("./models/task");
-const { ReplSet } = require("mongodb");
+const userRoutes = require("./routes/userRoutes");
+const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
@@ -10,72 +9,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post("/users", async (req, res) => {
-  console.log(req.body);
-  const user = new User(req.body);
-
-  try {
-    await user.save();
-    res.status(201).send(user);
-  } catch (e) {
-    res.status(400).send();
-  }
-});
-
-app.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-app.get("/tasks", async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.send(tasks);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-app.get("/tasks/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const task = await Task.findById(_id);
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.send(task);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-app.post("/tasks", async (req, res) => {
-  console.log(req.body);
-  const task = new Task(req.body);
-  try {
-    await task.save();
-    res.status(201).send(task);
-  } catch (e) {
-    res.status(400).send();
-  }
-});
+app.use(userRoutes);
+app.use(taskRoutes);
 
 app.listen(port, () => {
   console.log("app listening at port: " + port);
