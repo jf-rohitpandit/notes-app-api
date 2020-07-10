17b9f6cf3cdcd63c55cb8abc2640061c478ff803
rohit <rohit0311@outlook.com>
2020-07-10 16:50:02 +0530

    login

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,16 @@ const express = require("express");
 const User = require("../models/user");
 const router = express.Router();
 
+
+router.post('/users/login', async (req, res)=>{
+  try{
+    const user = await User.findByCredientials(req.body.email, req.body.password);
+    res.send(user);
+  }catch(e){
+    res.status(400).send()
+  }
+})
+
 router.post("/users", async (req, res) => {
   console.log(req.body);
   const user = new User(req.body);
@@ -46,10 +56,12 @@ router.patch("/users/:id", async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+
+    const user = await User.findById(req.params.id);
+    updates.forEach(update=> user[update]= req.body[update]);
+    await user.save();
+
+
     if (!user) {
       return res.status(404).send();
     }
