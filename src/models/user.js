3d464453a4cb8b6fe87bb71214ad62666ae1e42e
rohit <rohit0311@outlook.com>
2020-07-11 10:09:37 +0530

    authentication

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 
 const userSchema = new mongoose.Schema({
@@ -42,7 +43,36 @@ const userSchema = new mongoose.Schema({
       }
     },
   },
+  tokens:[{
+    token:{
+      type: String,
+      required: true
+    }
+  }]
 })
+
+
+userSchema.methods.toJSON = function(){
+  const user = this;
+
+  const userObject = user.toObject();
+  
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+}
+
+
+userSchema.methods.getAuthToken= async function(){
+  const user = this;
+  const token = jwt.sign({_id: user._id.toString()}, "thisismyproject");
+  user.tokens = user.tokens.concat({token});
+  user.save();
+  return token;
+}
+
+
 
 userSchema.statics.findByCredientials = async (email, password)=>{
   const user = await User.findOne({email});
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const User = require("../models/user");
+const auth = require('../middleware/auth');
 const router = express.Router();
 
 
 router.post('/users/login', async (req, res)=>{
   try{
     const user = await User.findByCredientials(req.body.email, req.body.password);
-    res.send(user);
+    const token = await user.getAuthToken();
+    res.send({user, token});
   }catch(e){
-    res.status(400).send()
+    res.status(400).send(e)
+  }
+})
+
+router.post('/users/logout', auth, async (req, res)=>{
+  try{
+    req.user.tokens = req.user.tokens.filter(token => token.token !== req.token);
+    await req.user.save();
+    res.send();
+  }catch(e){
+    res.status(500).send();
+  }
+})
+
+router.post('/users/logoutAll', auth, async (req, res)=>{
+  try{
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  }catch(e){
+    res.status(500).send();
   }
 })
 
@@ -17,36 +39,23 @@ router.post("/users", async (req, res) => {
   const user = new User(req.body);
 
   try {
+
     await user.save();
-    res.status(201).send(user);
+    const token = await user.getAuthToken();
+
+    res.status(201).send({user, token});
   } catch (e) {
-    res.status(400).send();
+    res.status(400).send(e);
   }
 });
 
-router.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
+router.get("/users/me",auth, async (req, res) => {
+  res.send(req.user);
 });
 
-router.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
 
-router.patch("/users/:id", async (req, res) => {
+
+router.patch("/users/me",auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowUpdates = ["name", "age", "password", "email"];
   const isValid = updates.every((update) => allowUpdates.includes(update));
@@ -57,29 +66,26 @@ router.patch("/users/:id", async (req, res) => {
 
   try {
 
-    const user = await User.findById(req.params.id);
-    updates.forEach(update=> user[update]= req.body[update]);
-    await user.save();
+    // const user = await User.findById(req.params.id);
+    updates.forEach(update=> req.user[update]= req.body[update]);
+    await req.user.save();
 
 
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
+    // if (!user) {
+    //   return res.status(404).send();
+    // }
+    res.send(req.user);
   } catch (e) {
     res.status(400).send();
   }
 });
 
-router.delete("/users/:id", async (req, res) => {
+router.delete("/users/me",auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
+    await req.user.remove();
+    res.send(req.user);
   } catch (e) {
-    res.status(500).send();
+    res.status(500).send(e);
   }
 });
 
