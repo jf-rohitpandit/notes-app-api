04d0961ad3f197d48a38201b4ce0d8099e4f3a2e
rohit <rohit0311@outlook.com>
2020-07-11 21:32:57 +0530

    image upload

--- src/models/user.js
+++ src/models/user.js
@@ -1,119 +1,122 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const Task = require('./task');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Task = require("./task");
 
-
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  age: {
-    type: Number,
-    default: 0,
-    required: true,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be positive");
-      }
-    },
-  },
-  email: {
-    type: String,
-    unique: true,
-    trim: true,
-    required: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Invalid email");
-      }
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(value) {
-      if (value.toLowerCase().includes("password")) {
-        throw new Error("password is included in password");
-      }
-    },
-  },
-  tokens:[{
-    token:{
+const userSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
-      required: true
-    }
-  }]
-},{
-  timestamps: true
-})
+      trim: true,
+      required: true,
+    },
+    age: {
+      type: Number,
+      default: 0,
+      required: true,
+      validate(value) {
+        if (value < 0) {
+          throw new Error("Age must be positive");
+        }
+      },
+    },
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 7,
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error("password is included in password");
+        }
+      },
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    avtar: {
+      type: Buffer,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-
-userSchema.virtual('tasks',{
-  ref:"Task",
+userSchema.virtual("tasks", {
+  ref: "Task",
   localField: "_id",
-  foreignField: "owner"
-})
+  foreignField: "owner",
+});
 
-userSchema.methods.toJSON = function(){
+userSchema.methods.toJSON = function () {
   const user = this;
 
   const userObject = user.toObject();
-  
+
   delete userObject.password;
   delete userObject.tokens;
+  delete userObject.avtar;
 
   return userObject;
-}
+};
 
-
-userSchema.methods.getAuthToken= async function(){
+userSchema.methods.getAuthToken = async function () {
   const user = this;
-  const token = jwt.sign({_id: user._id.toString()}, "thisismyproject");
-  user.tokens = user.tokens.concat({token});
+  const token = jwt.sign({ _id: user._id.toString() }, "thisismyproject");
+  user.tokens = user.tokens.concat({ token });
   user.save();
   return token;
-}
+};
 
+userSchema.statics.findByCredientials = async (email, password) => {
+  const user = await User.findOne({ email });
 
-
-userSchema.statics.findByCredientials = async (email, password)=>{
-  const user = await User.findOne({email});
-
-  if(!user){
-    throw new Error('Unable to login!');
+  if (!user) {
+    throw new Error("Unable to login!");
   }
 
   const isMatch = await bcrypt.compare(password, user.password);
 
-  if(!isMatch){
-    throw new Error('Unable to login!');
+  if (!isMatch) {
+    throw new Error("Unable to login!");
   }
 
   return user;
-}
+};
 
-
-userSchema.pre('save', async function(next){
+userSchema.pre("save", async function (next) {
   const user = this;
-  if(user.isModified('password')){
+  if (user.isModified("password")) {
     user.password = await bcrypt.hash(user.password, 8);
   }
   next();
-})
+});
 
-userSchema.pre('remove', async function(next){
+userSchema.pre("remove", async function (next) {
   const user = this;
 
-  await Task.deleteMany({owner: user._id});
+  await Task.deleteMany({ owner: user._id });
   next();
-})
+});
 
 const User = mongoose.model("User", userSchema);
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,75 +1,77 @@
 const express = require("express");
+const multer = require("multer");
+const sharp = require('sharp');
 const User = require("../models/user");
-const auth = require('../middleware/auth');
+const auth = require("../middleware/auth");
 const router = express.Router();
 
-
-router.post('/users/login', async (req, res)=>{
-  try{
-    const user = await User.findByCredientials(req.body.email, req.body.password);
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredientials(
+      req.body.email,
+      req.body.password
+    );
     const token = await user.getAuthToken();
-    res.send({user, token});
-  }catch(e){
-    res.status(400).send(e)
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send(e);
   }
-})
+});
 
-router.post('/users/logout', auth, async (req, res)=>{
-  try{
-    req.user.tokens = req.user.tokens.filter(token => token.token !== req.token);
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(
+      (token) => token.token !== req.token
+    );
     await req.user.save();
     res.send();
-  }catch(e){
+  } catch (e) {
     res.status(500).send();
   }
-})
+});
 
-router.post('/users/logoutAll', auth, async (req, res)=>{
-  try{
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
     req.user.tokens = [];
     await req.user.save();
     res.send();
-  }catch(e){
+  } catch (e) {
     res.status(500).send();
   }
-})
+});
 
 router.post("/users", async (req, res) => {
   console.log(req.body);
   const user = new User(req.body);
 
   try {
-
     await user.save();
     const token = await user.getAuthToken();
 
-    res.status(201).send({user, token});
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-router.get("/users/me",auth, async (req, res) => {
+router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
 
-
-
-router.patch("/users/me",auth, async (req, res) => {
+router.patch("/users/me", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowUpdates = ["name", "age", "password", "email"];
   const isValid = updates.every((update) => allowUpdates.includes(update));
+  const multer = require("multer");
 
   if (!isValid) {
     return res.status(400).send({ error: "Invalid fields" });
   }
 
   try {
-
     // const user = await User.findById(req.params.id);
-    updates.forEach(update=> req.user[update]= req.body[update]);
+    updates.forEach((update) => (req.user[update] = req.body[update]));
     await req.user.save();
-
 
     // if (!user) {
     //   return res.status(404).send();
@@ -80,12 +82,58 @@ router.patch("/users/me",auth, async (req, res) => {
   }
 });
 
-router.delete("/users/me",auth, async (req, res) => {
+router.delete("/users/me", auth, async (req, res) => {
   try {
     await req.user.remove();
     res.send(req.user);
   } catch (e) {
     res.status(500).send(e);
+  }
+});
+
+const upload = multer({
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(
+        new Error("Upload a photo with any of the jpg, jpeg, png format")
+      );
+    }
+    cb(undefined, true);
+  },
+});
+
+router.post("/users/me/avtar",auth,upload.single("avtar"),async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer();
+    req.user.avtar = buffer;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+router.delete("/users/me/avtar", auth, async (req, res) => {
+  req.user.avtar = undefined;
+  await req.user.save();
+  res.send();
+});
+
+router.get("/users/:id/avtar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user || !user.avtar) {
+      throw new Error();
+    }
+
+    res.set("content-type", "image/png");
+    res.send(user.avtar);
+  } catch (e) {
+    res.status(400).send();
   }
 });
 
